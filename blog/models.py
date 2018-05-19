19f5e62bbd86b57443001682e9485576bb13b8bf
kunal <kunalgk1999@gmail.com>
2018-05-19 19:21:34 +0530

    bug eith tinymce fixed

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from tinymce.models import HTMLfield
+from tinymce.models import HTMLField
 from datetime import datetime
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=1000)
-    intro = HTMLfield()
-    post = HTMLfield()
+    intro = HTMLField()
+    post = HTMLField()
     img_url = models.URLField(blank=True)
     date = models.DateField(default=datetime.today)
